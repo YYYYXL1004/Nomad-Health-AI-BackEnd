69ec5f89ff4f722a5759012511180515d2473160
YYYYXL <147621608+YYYYXL1004@users.noreply.github.com>
2025-05-14 13:27:19 +0800

    Update config.py
    更改API KEY 和 API SECRET

--- src/config/config.py
+++ src/config/config.py
@@ -18,8 +18,8 @@ class Config:
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp3', 'wav'} 
     
     # 讯飞语音识别API配置
-    XUNFEI_API_KEY = os.getenv('XUNFEI_API_KEY', '2cd533b30c559f0e94bc77d5c2d12e34')
-    XUNFEI_API_SECRET = os.getenv('XUNFEI_API_SECRET', 'MDJlZTJlNTQ0ZjJjYWEzOThjZGFjYjE2')
+    XUNFEI_API_KEY = os.getenv('XUNFEI_API_KEY', 'Your_API_KEY')
+    XUNFEI_API_SECRET = os.getenv('XUNFEI_API_SECRET', 'Your_API_SECRET')
     
     # 千问医疗模型API配置
     QWEN_API_URL = os.getenv('QWEN_API_URL', 'http://183.175.12.124:8000')
